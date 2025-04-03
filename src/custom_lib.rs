7cd1e26b3e463ea8f8fcb2d143b5b397c8b71a48
paultergust <prmoreira98@gmail.com>
2025-04-03 14:37:37 -0300

    Add creating of tilt project

---
+++ src/custom_lib.rs
@@ -0,0 +1,32 @@
+pub const CUSTOM_LIB: &str = r#"use serde::{Deserialize, Serialize};
+use tilt_app as tilt;
+
+#[derive(Deserialize)]
+pub struct Request {
+    pub arg: String,
+}
+
+#[derive(Serialize)]
+pub struct Response {
+    pub arg: String,
+}
+
+#[tilt::main]
+fn main(req: Request) -> Response {
+    Response { arg: req.arg }
+}
+"#;
+
+pub const CUSTOM_TOML: &str = r#"[package]
+name = "app"
+version = "0.1.0"
+edition = "2024"
+
+[lib]
+crate-type = ["cdylib"]
+
+[dependencies]
+serde = { version = "1.0.219", features = ["derive"] }
+serde_json = "1.0.140"
+tilt_app = { version = "0.1.0", path = "../tilt_app" }
+"#;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::{env, fs, process::Command};
 use clap::{Arg, Command as ClapCommand};
+use custom_lib::{CUSTOM_LIB, CUSTOM_TOML};
+use std::{fs, process::Command};
+mod custom_lib;
 
 fn main() {
     let matches = ClapCommand::new("tilt")
@@ -14,27 +16,42 @@ fn main() {
     if let Some(sub_matches) = matches.subcommand_matches("new") {
         let project_name = sub_matches.get_one::<String>("name").unwrap();
 
-        // Run `cargo new <project_name>`
-        let status = Command::new("cargo")
-            .arg("new")
-            .arg(project_name)
-            .status()
+        let output = Command::new("cargo")
+            .args(["new", "--lib", project_name])
+            .output()
             .expect("Failed to execute cargo new");
 
-        if !status.success() {
-            eprintln!("Failed to create project {}", project_name);
+        if !output.status.success() {
+            if let Some(101) = output.status.code() {
+                eprintln!("Error: Project '{}' already exists!", project_name);
+            } else {
+                eprintln!(
+                    "Error creating project '{}': {}",
+                    project_name,
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
             return;
         }
 
-        // Modify main.rs
-        let main_path = format!("{}/src/main.rs", project_name);
-        let custom_main = r#"fn main() {
-    println!("Hello from tilt");
-}"#;
-        
-        fs::write(&main_path, custom_main).expect("Failed to write main.rs");
+        let lib_path = format!("{}/src/lib.rs", project_name);
+        let toml_path = format!("{}/Cargo.toml", project_name);
+        let custom_lib = CUSTOM_LIB;
+        let custom_toml = CUSTOM_TOML;
+
+        // Add WebAssembly target
+        let status = Command::new("rustup")
+            .args(["target", "add", "wasm32-unknown-unknown"])
+            .status()
+            .expect("Failed to add wasm32-unknown-unknown target");
+
+        if !status.success() {
+            eprintln!("Failed to add WebAssembly target");
+        }
+
+        fs::write(&lib_path, custom_lib).expect("Failed to write lib.rs");
+        fs::write(&toml_path, custom_toml).expect("Failed to write Cargo.toml");
 
         println!("Project '{}' created successfully!", project_name);
     }
 }
-
