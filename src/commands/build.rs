a65300ce9316b93a4a877739d711c83ba5701706
burgossrodrigo <rodrigoburgos@live.com>
2026-02-26 13:44:33 +0000

    feat: build command module

---
+++ src/commands/build.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+use clap::Args;
+use std::process::Command;
+
+use crate::commands::Run;
+
+#[derive(Debug, Args)]
+pub struct Build;
+
+impl Run for Build {
+    async fn run(&self) -> Result<()> {
+        let mut child = Command::new("cargo")
+            .args(["build", "--target", "wasm32-wasip2", "--release"])
+            .spawn()
+            .expect("Failed to perform build. Do you have rust installed?");
+
+        let status = child.wait().expect("Failed to wait for build to complete");
+
+        if !status.success() {
+            anyhow::bail!("Cargo build failed");
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_build() {
+        let build = Build {};
+        let result = build.run().await;
+        assert!(result.is_ok());
+    }
+}
