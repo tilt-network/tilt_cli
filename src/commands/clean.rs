37731a34335bd3bdb490bfcff7fbc23b54a99b76
burgossrodrigo <rodrigoburgos@live.com>
2026-02-27 18:54:58 +0000

    feat: clean command module

---
+++ src/commands/clean.rs
@@ -0,0 +1,34 @@
+use crate::commands::Run;
+use anyhow::Result;
+use clap::Args;
+use std::process::Command;
+
+#[derive(Debug, Args)]
+pub struct Clean;
+
+impl Run for Clean {
+    async fn run(&self) -> Result<()> {
+        let mut child = Command::new("cargo")
+            .arg("clean")
+            .spawn()
+            .expect("Failed to execute cargo clean. Do you have rust installed");
+
+        let status = child.wait().expect("Failed to wait for cargo clean");
+
+        if !status.success() {
+            anyhow::bail!("Cargo clean failed")
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_clean() {
+        let clean = Clean;
+        assert!(clean.run().await.is_ok());
+    }
+}
