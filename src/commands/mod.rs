5e363cdfcca813d25f28450159f713739d37b36e
burgossrodrigo <rodrigoburgos@live.com>
2026-02-27 18:56:00 +0000

    feat: command mod

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,8 +1,15 @@
 mod build;
+mod clean;
+mod deploy;
+mod list;
 mod new;
+mod signin;
+mod test;
 
 use anyhow::Result;
 use clap::Subcommand;
+
+use crate::commands::build::Build;
 
 pub trait Run {
     async fn run(&self) -> Result<()>;
@@ -11,12 +18,24 @@ pub trait Run {
 #[derive(Debug, Subcommand)]
 pub enum Commands {
     New(new::New),
+    Build(build::Build),
+    Clean(clean::Clean),
+    Deploy(deploy::Deploy),
+    List(list::List),
+    Signin(signin::Signin),
+    Test(test::Test),
 }
 
 impl Commands {
     pub async fn run(&self) -> Result<()> {
         match self {
             Commands::New(command) => command.run().await,
+            Commands::Build(command) => command.run().await,
+            Commands::Clean(command) => command.run().await,
+            Commands::Deploy(command) => command.run().await,
+            Commands::List(command) => command.run().await,
+            Commands::Signin(command) => command.run().await,
+            Commands::Test(command) => command.run().await,
         }
     }
 }
