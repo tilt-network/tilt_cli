68b61ff2151378db8103c3b9a1ca1b9a89d48726
Yamuna <yamuna@tilt.technology>
2026-04-14 15:42:57 -0300

    WIP: add function build rust and build go

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -1,12 +1,21 @@
 use anyhow::{Context, Result};
 use clap::Args;
-use std::process::Command;
+use std::{env::args, process::Command};
+
+use crate::utils::{ProjectKind, detect_project_kind};
 
 #[derive(Debug, Args)]
 pub struct Build {}
 
 impl Build {
     pub async fn run(&self) -> Result<()> {
+    match detect_project_kind()? {
+        ProjectKind::Rust => self.build_rust(),
+        ProjectKind::Go => self.build_go(),
+    }
+    }
+
+    fn build_rust(&self) -> Result<()> {
         let mut child = Command::new("cargo")
             .args(["build", "--target", "wasm32-wasip2", "--release"])
             .spawn()
@@ -18,6 +27,28 @@ impl Build {
 
         if !status.success() {
             anyhow::bail!("Cargo build failed");
+        }
+        Ok(())
+    }
+
+    fn build_go(&self) -> Result<()> {
+        .args(["build", "-o", "tilt.wasm", "."])
+            .env("GOOS", "wasip1")
+            .env("GOARCH", "wasm")
+            .status()
+            .context("Failed to perfom build. Do you have Go installed?")?;
+        
+        if !status.success() {
+            anyhow::bail!("Go build failed");  
+        }
+        
+        match Command::new("wasm-tools")
+            .args(["component", "new", "tilt.wasm", "-o", "tilt:app@0.1.0.wasm"])
+            .status()
+        {
+            Ok(s) if s.success() => println!("Component wrapped: tilt:app@0.1.0.wasm"),
+            Ok(_) => anyhow::bail!("Failed to wrap component"),
+            Err(_) => anyhow::bail!("Failed to run wasm-tools"),
         }
         Ok(())
     }
